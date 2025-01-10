c2dbdf6e7fa41adc138d8a6003fb01fd4655ea46
WTiger <wtiger9218@gmail.com>
2025-01-10 18:08:58 -0500

    Update source

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders TODO List link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/TODO List/i);
   expect(linkElement).toBeInTheDocument();
 });
